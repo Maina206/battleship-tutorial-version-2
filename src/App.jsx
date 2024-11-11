6c7a5000339be0915bfe1bf476370f379c09535e
Alexander <maina206mwangi@gmail.com>
2024-11-11 22:55:13 +0300

    updating step 3 arranging ships

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import "./App.css";
-import Introduction from "./components/Introduction";
+import ArrangingShips from "./components/ArrangingShips";
 
 function App() {
   return (
     <>
       <h1>Hello I'm the battleship tutorial</h1>
       <h2>This is our Battleship project turned to react!</h2>
+
+      <ArrangingShips />
     </>
   );
 }
---
+++ src/components/Introduction.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../assets/styles/Introduction.css";
+
+function Introduction() {
+  return <div></div>;
+}
+
+export default Introduction;
