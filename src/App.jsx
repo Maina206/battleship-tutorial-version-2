9747d45cdc5e53190d1e21ec153f9c403ed1e2ba
arondi-12 <davieariwi@gmail.com>
2024-11-11 22:50:20 +0300

    intro and step 1

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 import "./App.css";
 import Introduction from "./components/Introduction";
 
+
 function App() {
   return (
     <>
       <h1>Hello I'm the battleship tutorial</h1>
       <h2>This is our Battleship project turned to react!</h2>
+      <Introduction />
     </>
   );
 }
