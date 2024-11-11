5538942e9ef89f5691a454d2c194da48dfe8d47d
assa mohammed <mohammedassad749@gmail.com>
2024-11-12 00:20:04 +0300

    created header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import "./App.css";
-import Introduction from "./components/Introduction";
+
+import HeaderComponent from "./Header/HeaderComponent";
+import FooterComponent from "./Footer/FooterComponent";
 
 function App() {
   return (
     <>
-      <h1>Hello I'm the battleship tutorial</h1>
-      <h2>This is our Battleship project turned to react!</h2>
+    <HeaderComponent/>
+    <FooterComponent/>
     </>
   );
 }
