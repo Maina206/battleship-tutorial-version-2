1c7d9c83266859f70ed78fdede843bcb04dc3c76
MMbats <petermbatia657@gmail.com>
2024-11-11 23:18:55 +0300

    Step2 and Step4

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
 import "./App.css";
+import Step2 from "./Step2/Step2";
+import Step4 from "./Step4/Step4";
 
 function App() {
   return (
     <>
       <h1>Hello I'm the battleship tutorial</h1>
       <h2>This is our Battleship project turned to react!</h2>
+<Step2/>
+<Step4/>
     </>
+  
   );
 }
 
